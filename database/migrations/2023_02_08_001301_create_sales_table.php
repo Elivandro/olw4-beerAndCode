ec6044b599eeb577d6ad12c9a0b0bac690e080c4
Elivandro <Elivandro@hotmail.com.br>
2023-02-08 18:22:23 -0400

    migrations created

---
+++ database/migrations/2023_02_08_001301_create_sales_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('seller_id')->constrained();
+            $table->foreignId('client_id')->constrained();
+            $table->timestamp('sold_at');
+            $table->char('status', 1);
+            $table->integer('total_amount');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+};
