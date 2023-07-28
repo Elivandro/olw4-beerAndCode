246dec1ad115b76bf6df8214ede11f65a282f243
Elivandro <Elivandro@hotmail.com.br>
2023-07-27 20:44:07 -0400

    dashboard and sales route created

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,18 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\ProfileController;
-use App\Models\SalesCommission;
-use Illuminate\Support\Facades\Route;
-use OpenAI\Laravel\Facades\OpenAI;
+use App\Http\Controllers\SaleController;
+use App\Http\Livewire\Dashboard;
 
 require __DIR__.'/auth.php';
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', 'dashboard');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', Dashboard::class)
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -22,15 +20,5 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     
     Route::Resource('/clients', ClientController::class);
-
-    Route::get('/chart', function (){
-    $fields = implode(',', SalesCommission::getColumns());
-    $question = "";
-
-    $chatgpt = OpenAI::completions()->create([
-            'model' => 'text-davinci-003',
-            'prompt' => 'me dê a formula da riqueza',
-            'max_tokens' => 1500
-        ])->choices[0]->text;
-    })->name("openai.chatgpt");
+    Route::Resource('/sales', SaleController::class)->only('index');
 });
