823ade13b259a2b9e9abcf19c532df9cc3178b14
Elivandro <Elivandro@hotmail.com.br>
2023-07-27 20:38:14 -0400

    client create view created

--- resources/views/clients/create.blade.php
+++ resources/views/clients/create.blade.php
@@ -1,5 +1,26 @@
 <x-app-layout>
     <x-slot name="header">
-        CRIAR
+        <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
+            Listagem de vendas
+        </h2>
     </x-slot>
+    <div class="flex justify-center py-12">
+        <div class="flex flex-col w-full max-w-xl p-12 border-2 border-gray-600 rounded-lg">
+            <div class="flex flex-col flex-1 mb-4">
+                <x-input-label for="name">Nome</x-input-label>
+                <x-text-input type="text" id="name" placeholder="nome do cliente" />
+            </div>
+            <div class="flex flex-col flex-1 mb-4">
+                <x-input-label for="email">Email</x-input-label>
+                <x-text-input type="email" id="email" placeholder="email do cliente" />
+            </div>
+            <div class="flex flex-col flex-1 mb-4">
+                <x-input-label for="address">Endereço</x-input-label>
+                <x-select  />                
+            </div>
+            <x-secondary-button class="ml-auto max-w-20">
+                Salvar
+            </x-secondary-button>
+        </div>
+    </div>
 </x-app-layout>
