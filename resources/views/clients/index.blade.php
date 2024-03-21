315b42698435c8e576468902d97655c5bd669b35
Elivandro <Elivandro@hotmail.com.br>
2024-03-21 12:35:19 -0400

    refactoring index table and eagerLoading created

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -1,23 +1,14 @@
 <x-app-layout>
     <div class="flex flex-col py-12 mx-8 items-between">
-        <x-heading 
-            title="Clientes"
-            description="Listagem de clientes"
-            btn-label="Cadastrar cliente"
-            :route="route('clients.create')"
-        />
+        <x-heading title="Clientes" description="Listagem de clientes" btn-label="Cadastrar cliente" :route="route('clients.create')" />
         <div class="w-full overflow-hidden md:rounded-lg">
-            <livewire:table
-                resource="Client"
-                :columns="[
-                    ['label' => 'Nome', 'column' => 'user.name'],
-                    ['label' => 'Email', 'column' => 'user.email'],
-                    ['label' => 'Cidade', 'column' => 'address.city'],
-                    ['label' => 'Estado', 'column' => 'address.state'],
-                ]"
-                edit="clients.edit"
-                delete="clients.destroy"
-            />
+            <livewire:table resource="Client" :columns="[
+                ['label' => 'Nome', 'column' => 'user.name'],
+                ['label' => 'Email', 'column' => 'user.email'],
+                ['label' => 'Cidade', 'column' => 'address.city'],
+                ['label' => 'Estado', 'column' => 'address.state'],
+            ]" :eager-loading="['user', 'address']" edit="clients.edit"
+                delete="clients.destroy" />
         </div>
     </div>
 </x-app-layout>
